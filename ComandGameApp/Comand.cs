126d6e0ec4e05f0ebf211f64fa97ec81501b9f28
emil.beyerov <emil.beyerov@digigo.work>
2024-11-04 20:09:20 +0400

    Connect with Db

--- ComandGameApp/Comand.cs
+++ ComandGameApp/Comand.cs
@@ -1,11 +1,13 @@
 ﻿using ComandGameApp.Bussiness.Abstract;
 using ComandGameApp.Bussiness.Concrete;
+using ComandGameApp.DataAccess.Util;
 using ComandGameApp.Entity;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace ComandGameApp
@@ -16,47 +18,46 @@ namespace ComandGameApp
         DataTable teamData1;
         DataTable teamData2;
         ITeamService teamService;
+        DbHelper dbHelper;
         public Comand()
         {
-            teamService = new TeamService(dataTable);
-            dataTable = new DataTable();
-            dataTable.Columns.Add("Team-1 name");
-            dataTable.Columns.Add("Team-1 Score");
-            dataTable.Columns.Add("Team-2 Score");
-            dataTable.Columns.Add("Team-2 name");
             InitializeComponent();
+
+            dbHelper = new DbHelper();
+            teamService = new TeamService(dataTable);
+
+            DataTable teams1 = dbHelper.ExecuteReader("SELECT Id, Name FROM Command");
+            DataTable teams2 = dbHelper.ExecuteReader("SELECT Id, Name FROM Command");
+
+            comboBox1.DataSource = teams1;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+
+            comboBox2.DataSource = teams2;
+            comboBox2.DisplayMember = "Name";
+            comboBox2.ValueMember = "Id";
+            MessageBox.Show("Completed");
+            GrdLoad();
             InitializeDataGridView();
         }
+
         private void InitializeDataGridView()
         {
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells; // Optional
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            //dataGridView1.Dock = DockStyle.Fill;
+        }
+
+        private void GrdLoad()
+        {
+            dataGridView1.DataSource = dbHelper.ExecuteReader(@"SELECT t1.Name AS Team1Name, g.TeamScore1, t2.Name AS Team2Name, g.TeamScore2
+                         FROM TeamScore g
+                         INNER JOIN Command t1 ON t1.Id = g.TeamId1
+                         INNER JOIN Command t2 ON t2.Id = g.TeamId2");
         }
 
         private void Comand_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = dataTable;
-            try
-            {
-                DataTable readData = teamService.GetTable();
-
-                teamData1 = readData.Copy();
-                teamData2 = readData.Copy();
-
-                comboBox1.DataSource = teamData1;
-                comboBox1.DisplayMember = "teamName";
-                comboBox1.ValueMember = "teamId";
-
-                comboBox2.DataSource = teamData2;
-                comboBox2.DisplayMember = "teamName";
-                comboBox2.ValueMember = "teamId";
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error loading teams: " + ex.Message);
-            }
+            
         }
 
 
@@ -64,14 +65,24 @@ namespace ComandGameApp
         {
             string scoreTeam1 = textBox1.Text;
             string scoreTeam2 = textBox2.Text;
-            string team1 = comboBox1.Text;
-            string team2 = comboBox2.Text;
+            string team1 = comboBox1.SelectedValue.ToString(); 
+            string team2 = comboBox2.SelectedValue.ToString(); 
 
-            dataTable.Rows.Add(team1, scoreTeam1, scoreTeam2, team2);
-            teamService.Add(
-                    dataTable
-                );
+            string query = @"INSERT INTO TeamScore (TeamId1, TeamScore1, TeamId2, TeamScore2) " +
+                       "VALUES (@TeamId1, @TeamScore1, @TeamId2, @TeamScore2)";
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@TeamId1", team1 },
+                { "@TeamScore1", scoreTeam1 },
+                { "@TeamId2", team2 },
+                { "@TeamScore2", scoreTeam2 }
+            };
+
+            dbHelper.ExecuteNonQuery(query, parameters);
+            GrdLoad();
             MessageBox.Show("Yeni komanda əlavə edildi.");
         }
+
     }
 }
--- ComandGameApp/Form1.cs
+++ ComandGameApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using ComandGameApp.DataAccess.Util;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,8 +18,10 @@ namespace ComandGameApp
     {
         string path = "C:\\Users\\LENOVO\\Desktop\\users.json.txt";
         DataTable dataTable;
+        DbHelper dbHelper;
         public Form1()
         {
+            dbHelper = new DbHelper();
             dataTable = new DataTable();
             dataTable.Columns.Add("username");
             dataTable.Columns.Add("password");
@@ -27,23 +30,52 @@ namespace ComandGameApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string readJson = File.ReadAllText(path);
-            DataTable readData = JsonConvert.DeserializeObject<DataTable>(readJson);
-
             string username = textBox1.Text;
             string password = textBox2.Text;
 
-            foreach (DataRow row in readData.Rows)
+            string query = "SELECT COUNT(1) FROM [dbo].[User] WHERE UserName = @username AND Password = @password";
+
+            var parameters = new Dictionary<string, object>
             {
-                if (row["username"].ToString() == username && row["password"].ToString() == password)
-                {
-                    MessageBox.Show("User successfully loggedIn");
-                    Comand comand = new Comand();
-                    comand.Show();
-                    this.Hide();
-                }
+                { "@username", username },
+                { "@password", password }
+            };
+
+            int userExists = dbHelper.ExecuteScalar(query, parameters);
+
+            if (userExists > 0)
+            {
+                MessageBox.Show("User successfully logged in");
+                Comand comand = new Comand();
+                comand.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
             }
         }
+
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    string readJson = File.ReadAllText(path);
+        //    DataTable readData = JsonConvert.DeserializeObject<DataTable>(readJson);
+
+        //    string username = textBox1.Text;
+        //    string password = textBox2.Text;
+
+        //    foreach (DataRow row in readData.Rows)
+        //    {
+        //        if (row["username"].ToString() == username && row["password"].ToString() == password)
+        //        {
+        //            MessageBox.Show("User successfully loggedIn");
+        //            Comand comand = new Comand();
+        //            comand.Show();
+        //            this.Hide();
+        //        }
+        //    }
+        //}
 
         private void Form1_Load(object sender, EventArgs e)
         {
